555ad0b8ec8fea1735789229b1f1c52c4eeb04e8
Laura Marulanda <lauris037.lm@gmail.com>
2023-11-29 22:04:45 -0500

    Crear usuario en base de datos

---
+++ controllers/auth.js
@@ -0,0 +1,37 @@
+const { response } = require("express");
+const Usuario = require("../models/Usuario");
+
+const crearUsuario = async (req, res = response) => {
+  //const { name, email, password } = req.body;
+  const usuario = new Usuario(req.body);
+  await usuario.save();
+
+  res.status(201).json({
+    ok: true,
+    msg: "registro",
+  });
+};
+
+const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body;
+
+  res.json({
+    ok: true,
+    msg: "login",
+    email,
+    password,
+  });
+};
+
+const revalidarToken = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "renew",
+  });
+};
+
+module.exports = {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { dbConnection } = require("./database/config");
+require("dotenv").config();
+
+//crear el servidor de express
+const app = express();
+
+//base de datos
+dbConnection();
+
+//directorio publico
+app.use(express.static("public"));
+
+//lectura y parseo del body
+//app.use middleware
+app.use(express.json());
+
+//Rutas
+app.use("/api/auth", require("./routes/auth"));
+
+//escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+});
