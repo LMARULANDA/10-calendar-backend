18b2fa1fdb2e5a1eba420e3341bb44a1b4bb219a
Laura Marulanda <lauris037.lm@gmail.com>
2023-11-29 23:05:40 -0500

    validaciones del usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,14 +2,37 @@ const { response } = require("express");
 const Usuario = require("../models/Usuario");
 
 const crearUsuario = async (req, res = response) => {
-  //const { name, email, password } = req.body;
-  const usuario = new Usuario(req.body);
-  await usuario.save();
+  
+  const { email, password } = req.body;
+
+  try{
+
+    let usuario = await Usuario.findOne({email});
+    console.log(usuario);
+
+    if(usuario) {
+      return res.status(400).json({
+        ok:false,
+        msg:'El usuario ya existe'
+      })
+    }
+
+    usuario = new Usuario(req.body);
+    await usuario.save();
 
   res.status(201).json({
     ok: true,
-    msg: "registro",
+    uid: usuario.id,
+    name: usuario.name
   });
+  }catch(error){
+    console.log(error);
+    return res.status(500).json(
+      {
+        ok:false,
+        msg: 'Error en el servidor'
+      });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
