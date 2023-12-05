50b901629f5662c9a3654760038fa7582d3aa5c0
Laura Marulanda <lauris037.lm@gmail.com>
2023-12-04 23:55:36 -0500

    modelo Evento

---
+++ controllers/events.js
@@ -0,0 +1,37 @@
+const { response } = require("express");
+
+const getEventos = (req,res = response) => {
+  
+   res.json({
+    ok: true,
+    message: "getEventos"
+  });
+};
+
+const crearEvento = (req, res = response) => {
+  res.json({
+    ok: true,
+    message: "crearEvento"
+  });
+};
+
+const actualizarEvento = (req, res = response) => {
+   res.json({
+    ok: true,
+    message: "actualizarEvento"
+  });
+};
+
+const eliminarEvento = (req, res = response) => {
+ res.json({
+    ok: true,
+    message: "eliminarEvento"
+  });
+};
+
+module.exports = {
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento,
+};
---
+++ routes/events.js
@@ -0,0 +1,31 @@
+//Todas tienen que pasar por la validacion del JWT
+
+/*Events route
+/api/events
+*/
+
+const { Router } = require("express");
+
+const { validarJWT } = require("../middlewares/validar-jwt");
+const {
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento,
+} = require("../controllers/events");
+
+const router = Router();
+router.use(validarJWT);
+//Obtener eventos
+router.get("/",getEventos);
+
+//Crear un nuevo evento
+router.post("/",crearEvento);
+
+//Actualizar evento
+router.put("/:id",actualizarEvento);
+
+//Borrar evento
+router.delete("/:id",eliminarEvento);
+
+module.exports = router;
